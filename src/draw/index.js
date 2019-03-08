3d1b1ed3b43688d24135050cb89342f62937edab
Jiashen <unbolibobo@foxmail.com>
2019-03-08 17:55:51 +0800

    项目暂存

--- src/draw/index.js
+++ src/draw/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import "./index.scss";
 import "flex.css"
+
 new Vue({
   el: "#app",
   template: require("./index.xtpl"),
@@ -8,7 +9,6 @@ new Vue({
     return {
       status: 0,
       num: 0,
-      endVal: 0,
       options: {
         useEasing: true,
         useGrouping: true,
@@ -17,38 +17,49 @@ new Vue({
         prefix: '',
         suffix: ''
       },
-      list: [
-        { id: 1, name: 'xiaoxiao', face: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1691771116,3692861469&fm=200&gp=0.jpg' },
-        { id: 2, name: 'xiaoxiao', face: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1691771116,3692861469&fm=200&gp=0.jpg' },
-        { id: 3, name: 'xiaoxiao', face: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1691771116,3692861469&fm=200&gp=0.jpg' },
-        { id: 4, name: 'xiaoxiao', face: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1691771116,3692861469&fm=200&gp=0.jpg' },
-        { id: 5, name: 'xiaoxiao', face: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1691771116,3692861469&fm=200&gp=0.jpg' },
-        { id: 6, name: 'xiaoxiao', face: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1691771116,3692861469&fm=200&gp=0.jpg' },
-        { id: 7, name: 'xiaoxiao', face: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1691771116,3692861469&fm=200&gp=0.jpg' }
-      ],
+      list: [],
       count: null,
+      tl: null,
+      index: 1,
+      interval: null
     }
   },
 
-  computed: {
-    maxList: function () {
-      return this.$data.list.slice(0, 5)
-    }
+  mounted() {
+    this.count = new CountUp('countNum', 0, this.num, 0, 3, this.options)
+    this.fresh()
+    this.interval = setInterval(() => {
+      this.fresh()
+    }, 10000)
   },
 
-  ready() {
-    this.count = new CountUp('countNum', 0, this.endVal , 0, 3, this.options);
-    this.count.start();
+  beforDestory() {
+    clearInterval(this.interval)
   },
 
   methods: {
     startDraw() {
-      this.count.update(this.endVal + 50);
-      this.endVal = this.endVal + 50
+
     },
     backPre() {
       this.status = 0;
+    },
+    fresh(num) {
+      let that = this;
+      return new Promise((resolve, reject) => {
+        fetch('http://my.12301.local/r/Face_Raffle/getList')
+          .then((response) => {
+            return response.json()
+          }).then((res) => {
+            if (res.code == 200) {
+              that.list = res.data.list;
+              that.count.update(res.data.count);
+              that.num = res.data.count
+            }
+          })
+      })
     }
+
   }
 });
 
